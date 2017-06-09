2bd4d37f6ffcf39a5c9c2fae09e11646fd2de634
Michael Campagnaro <mikecampo@gmail.com>
2017-06-09 18:24:41 -0400

    Switch to msys2 shell

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,4 +1,16 @@
 @echo off
+
+REM
+REM  To run this at startup, use this as your shortcut target:
+REM  %windir%\system32\cmd.exe /k w:\snake\misc\shell.bat
+REM
+
 call "X:\Programs\Visual Studio 15\VC\vcvarsall.bat" x64
 set path=w:\snake\misc;%path%
-"C:\Program Files\Git\git-bash.exe"
+set _NO_DEBUG_HEAP=1
+
+REM Start the editor
+REM call "X:\programs\4coder\4ed.exe" "X:\dev\games\projects\handmade-hero\code\build.sh" "-S"
+REM call "C:\Program Files\Git\git-bash.exe"
+call C:\msys64\msys2_shell.cmd -mingw64 -use-full-path
+exit
